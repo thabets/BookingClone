2ff3c926607ab636d1557b82156f21e3fd401adc
Sam Thabet <sallam.thabet@gmail.com>
2022-05-22 21:36:18 -0400

    authentication router for login and register

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -1,15 +1,9 @@
 import express from "express";
+import { login, register } from "../controllers/auth.js";
 
 const router = express.Router();
 
-router.get("/", (req, res) => {
-  res.send("hello, this is authentication page")
-})
+router.post("/register", register);
+router.post("/login", login);
 
-
-
-
-
-
-
-export default router
+export default router;
